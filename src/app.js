ce467dccc4cb192872ee67e913b4d9df227908de
littleDarkRose <mxxxpfeiffer@gmail.com>
2024-05-27 12:15:27 +0200

    Removed forecast from HTML and injected it to JS

--- src/app.js
+++ src/app.js
@@ -54,7 +54,33 @@ function manageSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>15º</strong>
+      </div>
+      <div class="weather-forecast-temperature">9º</div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", manageSearchSubmit);
 
 searchCity("Nairobi");
+displayForecast();
